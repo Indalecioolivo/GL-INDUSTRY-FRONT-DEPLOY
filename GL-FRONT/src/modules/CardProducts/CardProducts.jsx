de561c15a1b939afe22a8609725c18479f6654b9
Indalecio Olivo <indalecioolivo@gmail.com>
2024-10-01 10:14:44 -0300

    feat: Registered Products Module and card product created

--- GL-FRONT/src/modules/CardProducts/CardProducts.jsx
+++ GL-FRONT/src/modules/CardProducts/CardProducts.jsx
@@ -1,5 +1,12 @@
 import "./CardProducts.css";
+import photoIcon from "../../assets/photo-icon.png";
 
 export default function CardProducts() {
-  return <div className="card-container">Card</div>;
+  return (
+    <div className="card-container">
+      <img src={photoIcon} alt="" />
+      <h3>Kit Curly Active Desprogressiva cabelos cacheados</h3>
+      <p>Detalhes</p>
+    </div>
+  );
 }
--- GL-FRONT/src/modules/RegisteredProducts/RegisteredProducts.jsx
+++ GL-FRONT/src/modules/RegisteredProducts/RegisteredProducts.jsx
@@ -1,12 +1,17 @@
 import "./RegisteredProducts.css";
 import SearchModule from "../SearchModule/SearchModule";
 import CardProducts from "../CardProducts/CardProducts";
+import { db } from "../../services/db";
 
 export default function RegisteredProducts() {
   return (
     <section className="products-container">
       <SearchModule />
-      <CardProducts />
+      <div className="products-area">
+        {db.map((product) => (
+          <CardProducts key={product.id} />
+        ))}
+      </div>
     </section>
   );
 }
