76fdd0969c9df11ba3817f5ccfc924536cc174ef
Indalecio Olivo <indalecioolivo@gmail.com>
2024-10-27 13:17:08 -0300

    feat: registered products integrated on with api, now products seeimg are from database

--- GL-FRONT/src/pages/RegisteredProducts/RegisteredProducts.jsx
+++ GL-FRONT/src/pages/RegisteredProducts/RegisteredProducts.jsx
@@ -1,21 +1,22 @@
 import "./RegisteredProducts.css";
 import { GeneralContext } from "../../context/GeneralContext";
+import { db } from "../../services/db";
+import { useContext } from "react";
 import SearchModule from "../../modules/SearchModule/SearchModule";
 import CardProducts from "../../modules/CardProducts/CardProducts";
 import ModalToInformations from "../../modules/ModalToInformations/ModalToInformations";
 import ModalRegister from "../../modules/ModalRegister/ModalRegister";
-import { db } from "../../services/db";
-import { useContext } from "react";
 
 export default function RegisteredProducts() {
-  const { toModalInformations, showModalRegister } = useContext(GeneralContext);
+  const { toModalInformations, showModalRegister, productsData } =
+    useContext(GeneralContext);
   return (
     <section className="products-container">
       {toModalInformations.showModal ? <ModalToInformations /> : ""}
       {showModalRegister.showModal ? <ModalRegister /> : ""}
       <SearchModule />
       <div className="products-area">
-        {db.map((product) => (
+        {productsData.map((product) => (
           <CardProducts
             key={product.id}
             id={product.id}
