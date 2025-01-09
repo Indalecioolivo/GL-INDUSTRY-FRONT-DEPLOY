6c03cdb7d86a27a27312a2f54546f2abb37e1f25
Indalecio Olivo <indalecioolivo@gmail.com>
2025-01-09 10:08:58 -0300

    feat: formulario de informações de materia prima incluido ao modalregisterrawmaterial

--- GL-FRONT/src/modules/ModalToRegisterRawMaterial/ModalToRegisterRawMaterial.jsx
+++ GL-FRONT/src/modules/ModalToRegisterRawMaterial/ModalToRegisterRawMaterial.jsx
@@ -7,6 +7,35 @@ export default function ModalToRegisterRawMaterial() {
       <div className="modal-add-rawmaterial">
         <img src={CloseIcon} alt="" />
         <h2>Registrar Matéria Prima</h2>
+        <form action="">
+          <label htmlFor="productName">Nome</label>
+          <input id="productName" name="name" type="text" />
+          <label htmlFor="flowProductBarCode">Código de Barras</label>
+          <input
+            id="flowProductBarCode"
+            name="bar_code"
+            type="text"
+            maxLength={13}
+            // value={flowRegister.bar_code}
+            // onChange={(event) => handleFlowRegister(event)}
+            // onBlur={(event) => validateFields(event, event.target.value)}
+          />
+          <label htmlFor="flowProductAmount">Quantidade</label>
+          <input
+            type="number"
+            id="flowProductAmount"
+            name="amount"
+            // value={flowRegister.amount}
+            // onChange={(event) => handleFlowRegister(event)}
+            // onBlur={(event) => validateFields(event, event.target.value)}
+          />
+          <input
+            type="submit"
+            value="Registrar Fluxo"
+            className="button-register"
+            // onClick={(e) => validateFields(e, flowRegister)}
+          />
+        </form>
       </div>
     </div>
   );
